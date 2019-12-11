c1fe96b0fd7db0bfe60a43906370662338b693ea
Romina Manzano <romina.manzano@ecloudsolutions.com>
2019-12-11 15:22:41 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You were the chosen one!
         </p>
         <a
           className="App-link"
